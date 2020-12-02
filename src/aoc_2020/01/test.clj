5d9a85fad2e5a2c75ac9a3a81844781ee61b2aa4
Carl-Johan Lindblad <carl-johan.lindblad@svt.se>
2020-12-02 07:15:24 +0100

    Renamed require

--- src/aoc_2020/01/test.clj
+++ src/aoc_2020/01/test.clj
@@ -1,17 +1,17 @@
 (ns aoc-2020.01.test
   (:require [clojure.test :refer :all]
-            [aoc-2020.01.solution :as solution]
+            [aoc-2020.01.solution :as s1]
             [utils.input-parser :as parser]))
 
 (deftest day-01-test
   (testing "gets correct answer for part 1 test input"
-    (is (= 514579 (solution/part-1 [1721 979 366 299 675 1456]))))
+    (is (= 514579 (s1/part-1 [1721 979 366 299 675 1456]))))
   
   (testing "get correct solution for part 1"
-    (is (= 955584 (solution/part-1 (parser/read-numbers "src/aoc_2020/01/input.txt")))))
+    (is (= 955584 (s1/part-1 (parser/read-numbers "src/aoc_2020/01/input.txt")))))
   
   (testing "gets correct answer for part 2 test input"
-    (is (= 241861950 (solution/part-2 [1721 979 366 299 675 1456]))))
+    (is (= 241861950 (s1/part-2 [1721 979 366 299 675 1456]))))
   
   (testing "get correct solution for part 2"
-    (is (= 287503934 (solution/part-2 (parser/read-numbers "src/aoc_2020/01/input.txt"))))))
+    (is (= 287503934 (s1/part-2 (parser/read-numbers "src/aoc_2020/01/input.txt"))))))
