4aff1b85e6bb565e2cc01a3c7677b2e2818c9e0e
Carl-Johan Lindblad <carl-johan.lindblad@svt.se>
2020-12-07 11:46:08 +0100

    Day 7 solved

--- src/aoc_2020/07/solution.clj
+++ src/aoc_2020/07/solution.clj
@@ -1,9 +1,59 @@
 (ns aoc-2020.07.solution
-  (:require [utils.input-parser :as parser]
-            [clojure.string :as str]
-            [clojure.set :as set]))
+  (:require [clojure.string :as str]))
 
-(def test-input (parser/read-input "src/aoc_2020/07/test-input.txt" "\n\n"))
-(def input (parser/read-input "src/aoc_2020/07/input.txt" "\n\n"))
+(defn parse-bag [bag]
+  (let [matches (re-seq #"(\d+) (.*) bag" bag)]
+    (when matches
+      {:amount (second (first matches)) :type (last (first matches))})))
 
-(defn part-1 [input])
+(defn input->bag-map [input]
+  (reduce
+   (fn [acc cur]
+     (assoc
+      acc
+      (second (first (re-seq #"(.*) bag" (first (str/split cur #" contain ")))))
+      (map
+       parse-bag
+       (str/split (last (str/split cur #" contain ")) #", ")
+      )))
+   {}
+   input))
+
+(defn has-shiny-gold? [type bag-map]
+  (let [children (bag-map type)]
+    (cond
+      (= children '(nil))
+      false
+      (some #(= "shiny gold" (:type %)) children)
+      true
+      :else
+      (reduce
+       #(or %1 %2)
+       false
+       (for [child (keep identity children)]
+         (has-shiny-gold? (:type child) bag-map))))))
+
+(defn find-bags [bag-map]
+  (filter #(has-shiny-gold? % bag-map) (keys bag-map)))
+
+(defn part-1 [input]
+  (count (find-bags (input->bag-map input))))
+
+(defn bag-count [type bag-map]
+  (let [children (bag-map type)]
+    (cond
+      (= children '(nil))
+      1
+      :else
+      (reduce
+       #(+ %1 %2)
+       1
+       (for [child (keep identity children)]
+         (*
+          (Integer/parseInt (:amount child))
+          (bag-count (:type child) bag-map)))))))
+
+(defn part-2 [input]
+  (-
+   (bag-count "shiny gold" (input->bag-map input))
+   1))
--- src/aoc_2020/07/test.clj
+++ src/aoc_2020/07/test.clj
@@ -3,19 +3,19 @@
             [aoc-2020.07.solution :as s7]
             [utils.input-parser :as parser]))
 
-(def test-input (parser/read-input "src/aoc_2020/07/test-input.txt" "\n\n"))
-(def input (parser/read-input "src/aoc_2020/07/input.txt" "\n\n"))
+(def test-input (parser/read-input "src/aoc_2020/07/test-input.txt" "\n"))
+(def input (parser/read-input "src/aoc_2020/07/input.txt" "\n"))
 
 (deftest day-07-tests
    (testing "solves part 1 with test input"
-     (is (= 0 (s7/part-1 test-input))))
+     (is (= 4 (s7/part-1 test-input))))
   
-;;    (testing "solves part 1"
-;;      (is (= 0 (s7/part-1 input))))
+   (testing "solves part 1"
+     (is (= 274 (s7/part-1 input)))) ; 584 too high
   
-;;    (testing "solves part 2 with test input"
-;;      (is (= 0 (s7/part-2 test-input))))
+   (testing "solves part 2 with test input"
+     (is (= 32 (s7/part-2 test-input))))
   
-;;    (testing "solves part 2"
-;;      (is (= 0 (s7/part-2 input))))
+   (testing "solves part 2"
+     (is (= 158730 (s7/part-2 input))))
   )
