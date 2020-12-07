086b8fcb874f4098a1cb71b62fd27483c1e30fa4
Carl-Johan Lindblad <carl-johan.lindblad@svt.se>
2020-12-07 12:53:26 +0100

    Cleaned up day 7 a bit

--- src/aoc_2020/07/solution.clj
+++ src/aoc_2020/07/solution.clj
@@ -22,16 +22,10 @@
 (defn has-shiny-gold? [type bag-map]
   (let [children (bag-map type)]
     (cond
-      (= children '(nil))
-      false
-      (some #(= "shiny gold" (:type %)) children)
-      true
-      :else
-      (reduce
-       #(or %1 %2)
-       false
-       (for [child (keep identity children)]
-         (has-shiny-gold? (:type child) bag-map))))))
+      (= children '(nil)) false
+      (some #(= "shiny gold" (:type %)) children) true
+      :else (reduce or false (for [child children]
+                               (has-shiny-gold? (:type child) bag-map))))))
 
 (defn find-bags [bag-map]
   (filter #(has-shiny-gold? % bag-map) (keys bag-map)))
@@ -42,18 +36,11 @@
 (defn bag-count [type bag-map]
   (let [children (bag-map type)]
     (cond
-      (= children '(nil))
-      1
-      :else
-      (reduce
-       #(+ %1 %2)
-       1
-       (for [child (keep identity children)]
-         (*
-          (Integer/parseInt (:amount child))
-          (bag-count (:type child) bag-map)))))))
+      (= children '(nil)) 1
+      :else (reduce + 1 (for [child children]
+                          (*
+                           (Integer/parseInt (:amount child))
+                           (bag-count (:type child) bag-map)))))))
 
 (defn part-2 [input]
-  (-
-   (bag-count "shiny gold" (input->bag-map input))
-   1))
+  (- (bag-count "shiny gold" (input->bag-map input)) 1))
