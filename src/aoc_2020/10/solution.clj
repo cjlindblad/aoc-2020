cd490ef74073c7470c85a208cbfa94d3207fe8da
Carl-Johan Lindblad <carl-johan.lindblad@svt.se>
2020-12-10 13:25:58 +0100

    Refactored day 10

--- src/aoc_2020/10/solution.clj
+++ src/aoc_2020/10/solution.clj
@@ -2,25 +2,29 @@
   (:require [clojure.test :refer :all]
             [clojure.math.numeric-tower :as math]))
 
+(defn with-ends [input]
+  (let [sorted-input (sort input)]
+    (concat (conj sorted-input 0) [(+ 3 (last sorted-input))])))
+
 (defn get-diffs [input]
   (map
    #(- (last %) (first %))
-   (partition 2 1 (sort input))))
+   (partition 2 1 (with-ends input))))
 
 (defn part-1 [input]
   (let [diffs (get-diffs input)]
-    (* (+ 1 (count (filter #(= % 1) diffs)))
-       (+ 1 (count (filter #(= % 3) diffs))))))
+    (* (count (filter #(= % 1) diffs))
+       (count (filter #(= % 3) diffs)))))
 
-(defn map-count [input]
-  (let [input (sort (conj input 0))]
+(defn map-options [input]
+  (let [input (with-ends input)]
     (map-indexed
      (fn [idx item]
        (count (filter #(<= % (+ 3 item)) (take 3 (drop (inc idx) input)))))
      input)))
 
 (defn part-2 [input]
-  (let [options-string (apply str (map-count input))
+  (let [options-string (apply str (map-options input))
         sevens (count (re-seq #"332" options-string))
         fours (count (re-seq #"132" options-string))
         twos (count (re-seq #"121" options-string))]
